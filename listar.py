1b14a9ec940bebc292ab3342633fd12496c3c0c3
Fernando <fernando.scheibler71@aluno.ifce.edu.br>
2026-01-04 11:39:58 -0300

    Butao de deletar e editar adicionados, falta integrar o vizualizar com o codigo de listar especifico do daivizon

--- listar.py
+++ listar.py
@@ -53,11 +53,71 @@ def visualizar_cliente():
     cliente = cliente_selecionado()
     if not cliente:
         messagebox.showwarning("Aviso","Selecione um cliente")
-        return messagebox.showinfo(
+        return None 
+    elif cliente:
+        messagebox.showinfo(
         "Cliente",
         f"ID: {cliente[0]}\nNome: {cliente[1]}\nCPF: {cliente[2]}\nEmail: {cliente[3]}"
     )
 
+def deleta_cliente():
+    cliente = cliente_selecionado()
+    if not cliente:
+        messagebox.showwarning("Por favor, selecione algum cliente antes de prosseguir ")
+        return 
+
+    if messagebox.askyesno("Confirmar", "Deseja realmente deletar este cliente?"):
+        conexao = conectar()
+        cursor = conexao.cursor()
+        cursor.execute("DELETE FROM clientes WHERE id = %s", (cliente[0],))
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+        listar_clientes()
+
+def editar_clientes():
+    cliente = cliente_selecionado()
+    if not cliente:
+        messagebox.showwarning("Por favor, selecione algum cliente antes de prosseguir")
+        return 
+
+    janela_editar = tk.Toplevel(janela)
+    janela_editar.title("Edição")
+    janela_editar.geometry("300x200")
+    
+    
+    entrada_nome = tk.Entry(janela_editar)
+    entrada_nome.pack()
+    entrada_nome.insert(0, cliente[1])
+
+    entrada_email = tk.Entry(janela_editar)
+    entrada_email.pack()
+    entrada_email.insert(0, cliente[3])
+
+    def salvar():
+        a_nome = entrada_nome.get()
+        a_email = entrada_email.get()
+    
+        conexao = conectar()
+        cursor = conexao.cursor()
+
+        cursor.execute("UPDATE clientes SET nome =%s, email = %s WHERE id = %s", (a_nome, a_email, cliente[0]))
+    
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+
+        listar_clientes()
+        janela_editar.destroy()
+        messagebox.showinfo("Sucesso", "Cliente alterado com sucesso")
+    tk.Button(janela_editar, text="Salvar", command = salvar).pack(pady = 10) 
+
+frame_butoes = tk.Frame(janela)
+frame_butoes.pack(pady = 5)
+
+botao_Vizualizar = tk.Button( frame_butoes, text="vizualizar cliente", command= visualizar_cliente).pack(side = "left",padx = 5)
+botao_editar = tk.Button(frame_butoes, text="Editar", command = editar_clientes).pack(side = "left", padx = 5)
+botao_deletar = tk.Button(frame_butoes, text= "Deletar", command = deleta_cliente).pack(side = "left", padx = 5)
 
 listar_clientes()
 janela.mainloop()
